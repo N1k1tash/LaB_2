da3668b1e0d63f544802be9e6d26467e3df23879
N1k1tash <112692853+N1k1tash@users.noreply.github.com>
2022-12-30 07:52:44 +0300

    Create main.cpp

---
+++ 2/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include "modAlphaCipher.h"
+using namespace std;
+string cipher(string str, const int key);
+string decipher(string str, const int key);
+int main()
+{
+    Cipher cpr;
+    int number = 7;
+    try {
+        string original = "MAZZERATI";
+        string encrypted = cpr.cipher(original, number);
+        string decrypted = cpr.decipher(encrypted, number);
+        cout << original << endl;
+        cout << encrypted << endl;
+        cout << decrypted << endl;
+    } catch (const cipher_error & e) {
+        cerr << "Error: " << e.what() << endl;
+    }
+    return 0;
+}
